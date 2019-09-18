d4fd168a0359da6a7ec6fd592ad7ddc346f0e0af
Evgenii Morenkov <yarad8613@gmail.com>
2019-09-18 03:52:36 +0200

    Upgrade qor formula to v0.7.4

--- qor.rb
+++ qor.rb
@@ -1,8 +1,8 @@
 class Qor < Formula
    desc "Cli v2 (using Go)"
    homepage "https://github.com/Qordobacode/Cli-v2"
-   url "https://github.com/Qordobacode/Cli-v2/archive/v0.7.3.tar.gz"
-   sha256 "1f593ba6e63c2eac8229cd1e27ae62ea6ea27ec78ea344aca80b854c3e764b93"
+   url "https://github.com/Qordobacode/Cli-v2/archive/v0.7.4.tar.gz"
+   sha256 "469ea2c705844cdc07f379addc788504dc228f87586c7d62203302367e09e161"
    head "https://github.com/Qordobacode/Cli-v2.git"
 
    depends_on "go" => :build
@@ -12,7 +12,7 @@ class Qor < Formula
      src = buildpath/"src/github.com/qordobacode/cli-v2"
      src.install buildpath.children
      src.cd do
-       system "go", "build", "-mod", "vendor", "-ldflags", "\"-w -s -X 'github.com/qordobacode/cli-v2/cmd/info.VersionFlag=0.7.3'\"", "-o", "#{bin}/qor"
+       system "go", "build", "-mod", "vendor", "-ldflags", "\"-w -s -X 'github.com/qordobacode/cli-v2/cmd/info.VersionFlag=0.7.4'\"", "-o", "#{bin}/qor"
        prefix.install_metafiles
      end
    end
