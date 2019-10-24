99b0974df8a2b05df147c1424486845f8010177e
Evgenii Morenkov <yarad8613@gmail.com>
2019-10-24 12:21:23 +0300

    Update qor formula to version v0.8.2

--- qor.rb
+++ qor.rb
@@ -1,8 +1,8 @@
 class Qor < Formula
    desc "Cli v2 (using Go)"
    homepage "https://github.com/Qordobacode/Cli-v2"
-   url "https://github.com/Qordobacode/Cli-v2/archive/v0.8.1-alpha1.tar.gz"
-   sha256 "a2b83e0e4dbcafbaf7b45f7c258be1d52f63f7edd72898f5be58cf2b0fd4bd4b"
+   url "https://github.com/Qordobacode/Cli-v2/archive/v0.8.2.tar.gz"
+   sha256 "42467f36ea72564c464e9748e7f962e2db88cfcd1be9dadb0ff4b7b209b79dda"
    head "https://github.com/Qordobacode/Cli-v2.git"
 
    depends_on "go" => :build
@@ -12,7 +12,7 @@ class Qor < Formula
      src = buildpath/"src/github.com/qordobacode/cli-v2"
      src.install buildpath.children
      src.cd do
-       system "go", "build", "-mod", "vendor", "-ldflags", "\"-w -s -X 'github.com/qordobacode/cli-v2/cmd/info.VersionFlag=v0.8.1-alpha1'\"", "-o", "#{bin}/qor"
+       system "go", "build", "-mod", "vendor", "-ldflags", "\"-w -s -X 'github.com/qordobacode/cli-v2/cmd/info.VersionFlag=v0.8.2'\"", "-o", "#{bin}/qor"
        prefix.install_metafiles
      end
    end
